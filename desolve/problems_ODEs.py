fixed the Jacobian in Barnes

desolve/problems_ODEs.py
@@ -410,16 +410,18 @@ class Barnes:
         p1=ctx['p1']
         p2=ctx['p2']
         p3=ctx['p3']
-        
+
         u_out=np.zeros((2,))
         u_out[0]=p1*u_in[0]-p2*u_in[0]*u_in[1]
         u_out[1]=p2*u_in[0]*u_in[1]-p3*u_in[1]
         
         j_out=np.zeros((2,2))
+        
         j_out[0,0]=p1-p2*u_in[1]
         j_out[0,1]=-p2*u_in[0]
         j_out[1,0]=p2*u_in[1]
-        j_out[0,1]=p2*u_in[0]-p3
+        j_out[1,1]=p2*u_in[0]-p3
+        
         return u_out,j_out
 
     def initial_solution(self):